39dff2edecb12e08af88aafec1e71850c1727a10
om patel <om28patel2004@gmail.com>
2024-02-15 01:58:18 +0530

    feat:Paytm docs implementation

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,10 +5,77 @@ import { IoIosCloseCircleOutline } from "react-icons/io";
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 import { IoBagCheckOutline } from "react-icons/io5";
 import Link from "next/link";
-
+import Head from "next/head";
+import Script from "next/script";
 const checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
+  const initiatePayment = async () => {
+    let txnToken;
+    let amount;
+    // get transaction token
+    let a = await fetch(`${NEXT_PUBLIC_HOST}/api/pretransaction`);
+    const data = { cart, subTotal };
+    async function postJSON(data) {
+      try {
+        const response = await fetch("https://example.com/profile", {
+          method: "POST", // or 'PUT'
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+        const result = await response.json();
+        console.log("Success:", result);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    postJSON(data);
+
+    var config = {
+      root: "",
+      flow: "DEFAULT",
+      data: {
+        orderId: Math.random() /* update order id */,
+        token: txnToken /* update token value */,
+        tokenType: "TXN_TOKEN",
+        amount: amount /* update amount */,
+      },
+      handler: {
+        notifyMerchant: function (eventName, data) {
+          console.log("notifyMerchant handler function called");
+          console.log("eventName => ", eventName);
+          console.log("data => ", data);
+        },
+      },
+    };
+
+    // initialze configuration using init method
+    window.Paytm.CheckoutJS.init(config)
+      .then(function onSuccess() {
+        // after successfully updating configuration, invoke JS Checkout
+        window.Paytm.CheckoutJS.invoke();
+      })
+      .catch(function onError(error) {
+        console.log("error => ", error);
+      });
+  };
   return (
     <div className=" bg-custom-skin">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"
+        />
+      </Head>
+      <Script
+        type="application/javascript"
+        crossorigin="anonymous"
+        src={`${process.env.PAYTM_HOST}/merchantpgui/checoutjs/merchants/${process.env.PAYTM_MID}.js`}
+        onLoad="onScriptLoad()"
+      ></Script>
+
       <h1 className=" font-bold text-2xl text-center py-8">Checkout</h1>
       <h2 className=" text-center ">Delivery Details</h2>
       <div className=" mx-auto">
@@ -113,7 +180,10 @@ const checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
             </ol>
             <div className=" flex justify-center items-center py-8">
               <span className=" w-1/3  ">subTotal : ₹{subTotal}</span>
-              <button className=" my-2 w-1/3  bg-orange-500  flex text-center hover:bg-orange-700 text-white font-semibold hover:text-white py-2 px-4 border border-orange-700 hover:border-transparent rounded-lg">
+              <button
+                onClick={initiatePayment}
+                className=" my-2 w-1/3  bg-orange-500  flex text-center hover:bg-orange-700 text-white font-semibold hover:text-white py-2 px-4 border border-orange-700 hover:border-transparent rounded-lg"
+              >
                 <IoBagCheckOutline size={20} /> PAY ₹{subTotal}
               </button>
             </div>
