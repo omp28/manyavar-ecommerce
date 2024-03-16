ffdf6e0664f888e7b627219dc946c2e34e033f66
om patel <om28patel2004@gmail.com>
2024-03-16 19:21:42 +0530

    feat:chakraUI for cart

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,6 +10,16 @@ import { IoBagCheckOutline } from "react-icons/io5";
 import { FaCartShopping } from "react-icons/fa6";
 import { IoLogIn } from "react-icons/io5";
 import { useRouter } from "next/router";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  useDisclosure,
+  useOutsideClick,
+} from "@chakra-ui/react";
 
 const Nav = ({
   Logout,
@@ -22,21 +32,23 @@ const Nav = ({
 }) => {
   const [dropdown, setDropdown] = useState(false);
   const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-  const ref = useRef();
+  const ref = useRef(null);
   const router = useRouter();
 
-  const toggleCart = ({}) => {
-    const cartSidebar = ref.current; // Get the element
-    if (cartSidebar) {
-      // Safety check
-      cartSidebar.classList.toggle("hidden");
-      cartSidebar.classList.toggle("translate-x-0");
-    } else if (!ref.current?.classList.contains("hidden")) {
-      ref.current?.classList.remove("translate-x-0");
-      ref.current?.classList.add("hidden");
-    }
+  const toggleCart = () => {
+    isOpen ? onClose() : onOpen();
   };
+
+  useOutsideClick({
+    ref: ref,
+    handler: () => {
+      onClose();
+    },
+  });
+
   useEffect(() => {
     const hideSidebarRoutes = ["/orders", "/login", "/checkout"];
     if (hideSidebarRoutes.includes(router.pathname)) {
@@ -44,23 +56,10 @@ const Nav = ({
     } else {
       setIsSidebarVisible(true);
     }
-    const cartSidebar = ref.current;
-    if (cartSidebar) {
-      if (Object.keys(cart).length > 0) {
-        ref.current.classList.remove("hidden");
-        ref.current.classList.add("translate-x-0");
-      } else {
-        ref.current.classList.remove("translate-x-0");
-        ref.current.classList.add("hidden");
-      }
-    }
   }, [cart, router]);
+
   return (
-    <div
-      className={
-        isSidebarVisible ? "sidebar duration-500 overflow-x-hidden" : ""
-      }
-    >
+    <div className="sidebar duration-500 overflow-x-hidden ">
       <div className="flex-col bg-red-950">
         <div className="bg-custom-skin">
           <div className="flex justify-center items-center py-2">
@@ -148,102 +147,102 @@ const Nav = ({
                 )}
               </button>
               {/* SIDECART  */}
-              {isSidebarVisible && (
-                <div
-                  ref={ref}
-                  className={
-                    " max-w-80 z-10 flex-col sidebar absolute top-0 right-0 bg-custom-skin p-10   border-4 border-red-950  "
-                  }
-                >
-                  <h1 className="flex justify-center items-center py-2 text-2xl text-red-900">
-                    YOUR CART
-                  </h1>
-                  <div
-                    onClick={toggleCart}
-                    className=" text-3xl bg-red-800 cursor-pointer rounded-full absolute top-2 right-2"
-                  >
-                    <IoIosCloseCircleOutline />
-                  </div>
-                  <ol>
-                    {Object.keys(cart).length === 0 && (
-                      <div>
-                        <h1>Cart is EMPTY !</h1>
-                        <h1>Add items to checkout</h1>
-                      </div>
-                    )}
-                    {Object.keys(cart).map((k) => (
-                      <li key={k} className=" list-decimal">
-                        <div className="flex font-semibold my-1">
-                          <div className="   text-start  w-2/3">
-                            <h1 className="overflow-hidden overflow-ellipsis whitespace-nowrap max-w-[100%] max-h-9 ">
-                              {cart[k].name}
-                            </h1>
-                            <h1>
-                              ( {cart[k].variant} / {cart[k].size} )
-                            </h1>
-                          </div>
-                          <div className=" flex text-center justify-around  w-1/3">
-                            <button>
-                              <AiOutlineMinusCircle
-                                onClick={() => {
-                                  removeFromCart(
-                                    k,
-                                    1,
-                                    cart[k].price,
-                                    cart[k].name,
-                                    cart[k].size,
-                                    cart[k].variant
-                                  );
-                                }}
-                                className="text-red-800"
-                                size={20}
-                              />
-                            </button>
-                            <h1 className=" items-center flex">
-                              {cart[k].qty}
-                            </h1>
 
-                            <button>
-                              <AiOutlinePlusCircle
-                                onClick={() => {
-                                  addToCart(
-                                    k,
-                                    1,
-                                    cart[k].price,
-                                    cart[k].name,
-                                    cart[k].size,
-                                    cart[k].variant
-                                  );
-                                }}
-                                className="text-red-800"
-                                size={20}
-                              />
-                            </button>
-                          </div>
+              <Drawer
+                isOpen={isOpen}
+                placement="left"
+                onClose={onClose}
+                finalFocusRef={ref}
+              >
+                <DrawerOverlay />
+
+                <DrawerContent>
+                  <DrawerBody className=" translate-x-0 border-2 border-gray-300 rounded-lg w-96 bg-custom-skin h-full scroll-auto py-4 px-8 ">
+                    <div className=" flex justify-end">
+                      <DrawerCloseButton className=" right-0  " />
+                    </div>
+                    <h1 className=" font-bold text-lg">Your Cart</h1>
+                    <ol>
+                      {Object.keys(cart).length === 0 && (
+                        <div>
+                          <h1>Cart is EMPTY !</h1>
+                          <h1>Add items to checkout</h1>
                         </div>
-                      </li>
-                    ))}
-                  </ol>
-                  <span className=" w-1/3  ">subTotal : ₹{subTotal}</span>
-                  {/* checkout */}
-                  <Link href="/checkout">
-                    <button
-                      disabled={Object.keys(cart).length === 0 || !user.value}
-                      className=" disabled:bg-orange-200 my-2 w-full  bg-orange-500  flex text-center hover:bg-orange-700 text-white font-semibold hover:text-white py-2 px-4 border border-orange-700 hover:border-transparent rounded-lg"
-                    >
-                      <IoBagCheckOutline size={20} /> CheckOut
-                    </button>
-                  </Link>
+                      )}
+                      {Object.keys(cart).map((k) => (
+                        <li key={k} className=" list-decimal">
+                          <div className="flex font-semibold my-1">
+                            <div className="   text-start  w-2/3">
+                              <h1 className="overflow-hidden overflow-ellipsis whitespace-nowrap max-w-[100%] max-h-9 ">
+                                {cart[k].name}
+                              </h1>
+                              <h1>
+                                ( {cart[k].variant} / {cart[k].size} )
+                              </h1>
+                            </div>
+                            <div className=" flex text-center justify-around  w-1/3">
+                              <button>
+                                <AiOutlineMinusCircle
+                                  onClick={() => {
+                                    removeFromCart(
+                                      k,
+                                      1,
+                                      cart[k].price,
+                                      cart[k].name,
+                                      cart[k].size,
+                                      cart[k].variant
+                                    );
+                                  }}
+                                  className="text-red-800"
+                                  size={20}
+                                />
+                              </button>
+                              <h1 className=" items-center flex">
+                                {cart[k].qty}
+                              </h1>
 
-                  <button
-                    disabled={Object.keys(cart).length === 0}
-                    onClick={clearCart}
-                    className="disabled:bg-orange-200 my-2 w-full  bg-orange-500  flex text-center hover:bg-orange-700 text-white font-semibold hover:text-white py-2 px-4 border border-orange-700 hover:border-transparent rounded-lg"
-                  >
-                    Clear Cart
-                  </button>
-                </div>
-              )}
+                              <button>
+                                <AiOutlinePlusCircle
+                                  onClick={() => {
+                                    addToCart(
+                                      k,
+                                      1,
+                                      cart[k].price,
+                                      cart[k].name,
+                                      cart[k].size,
+                                      cart[k].variant
+                                    );
+                                  }}
+                                  className="text-red-800"
+                                  size={20}
+                                />
+                              </button>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ol>
+                    <span className=" w-1/3  ">subTotal : ₹{subTotal}</span>
+                    {/* checkout */}
+                    <Link href="/checkout">
+                      <button
+                        disabled={Object.keys(cart).length === 0 || !user.value}
+                        className=" disabled:bg-orange-200 my-2 w-full  bg-orange-500  flex text-center hover:bg-orange-700 text-white font-semibold hover:text-white py-2 px-4 border border-orange-700 hover:border-transparent rounded-lg"
+                      >
+                        <IoBagCheckOutline size={20} /> CheckOut
+                      </button>
+                    </Link>
+
+                    <button
+                      disabled={Object.keys(cart).length === 0}
+                      onClick={clearCart}
+                      className="disabled:bg-orange-200 my-2 w-full  bg-orange-500  flex text-center hover:bg-orange-700 text-white font-semibold hover:text-white py-2 px-4 border border-orange-700 hover:border-transparent rounded-lg"
+                    >
+                      Clear Cart
+                    </button>
+                  </DrawerBody>
+                </DrawerContent>
+              </Drawer>
             </div>
           </div>
         </div>
